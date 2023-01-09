189f1395600d906f587c19ba21ae46ef32879ab9
Dev4LifeV <vctudo.27@gmail.com>
2023-01-09 14:13:36 -0300

    ✨ implemented google sign in

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -10,25 +10,21 @@ import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+
        validateAuth()
     }
-    
+
     private func validateAuth() {
-        
-        if FirebaseAuth.Auth.auth().currentUser == nil {
+
+        if FirebaseAuth.Auth.auth().currentUser != nil {
+            dismiss(animated: true)
+        } else {
             let vc = LoginViewController()
             let nav = UINavigationController(rootViewController: vc)
             nav.modalPresentationStyle = .fullScreen
             present(nav, animated: false)
-        } else {
-            
         }
     }
     
